9599318e9d21bc0beba643f7a72ec5647cd81e11
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-22 20:51:51 +0600

    change in repo

--- src/Redux/detailcounter/actionTypes.js
+++ src/Redux/detailcounter/actionTypes.js
@@ -5,9 +5,5 @@ export const ADD_OPTIONS4 = 'addOptions4';
 export const ADD_OPTIONS5 = 'addOptions5';
 export const BOOK = 'book';
 export const ADD_ROWS = 'add_rows';
-export const HANDLE_OPTIONCHANGE = 'handle_optionchange';
-export const HANDLE_OPTIONCHANGE2 = 'handle_optionchange2';
-export const HANDLE_OPTIONCHANGE3 = 'handle_optionchange3';
-export const HANDLE_OPTIONCHANGE4 = 'handle_optionchange4';
-export const HANDLE_OPTIONCHANGE5 = 'handle_optionchange5';
+
 
